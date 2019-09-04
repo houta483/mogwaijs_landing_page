1a5cf9f4943ca367b402c080efbff10decedba2d
Tanner Houghton <houta483@uchicago.edu>
2019-09-03 19:18:49 -0700

    initial git commit

---
+++ src/pages/documentation.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default () => (
+  <Layout>
+    <h1>Documentation</h1>
+    <b>Table of Contents</b>
+    <ul>
+      <li>Installation</li>
+    </ul>
+  </Layout>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,11 @@
 import React from "react"
+import Layout from "../components/layout"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <Layout>
+    <h1>Welcome to MogwaiJS</h1>
+    <p>
+    MogwaiJS enables developers to query a Gremlin server in JavaScript using the Cypher Query Language.
+    </p>
+  </Layout>
+)
